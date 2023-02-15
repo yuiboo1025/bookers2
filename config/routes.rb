95282ecc3942d9e1c45652f6f20a49884a76b9b5
yuiboo1025 <122716372+yuiboo1025@users.noreply.github.com>
2023-02-15 10:06:10 +0000

    [Add]削除機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'homes/top'
+  resources :books,only: [:new,:index,:show,:create,:destroy]
+  
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.
   root to: "homes#top"
+  get 'home/about' => 'homes#about',as:'about'
+  
 end
