b34032ca06b421dabf6ce6532f341d7dea7d55c9
yuiboo1025 <122716372+yuiboo1025@users.noreply.github.com>
2023-02-16 09:31:58 +0000

    [Add]userモデル、ビューの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -31,7 +31,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     @book.update(book_params)
-    redirect_to books_path
+    redirect_to book_path(@book.id)
   end
   
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :books,only: [:new,:index,:show,:create,:destroy,:edit,:update]
-  
   devise_for :users
+  resources :books,only: [:new,:index,:show,:create,:destroy,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.
   root to: "homes#top"
   get 'home/about' => 'homes#about',as:'about'
